a34796bef87c9f0bf31669e3974ce227b3ecd37f
prashanthkulal12345 <kulalprashanth779@gmail.com>
2026-07-04 09:39:23 +0530

    learned about Date and its some methods

---
+++ Javascript/Date.js
@@ -0,0 +1,10 @@
+let newDate=new Date();
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+console.log(newDate.getFullYear());
+console.log(newDate.getHours());
+console.log(newDate.getMonth());
+console.log(newDate.toJSON());
+console.log(newDate.toLocaleDateString());
+console.log(newDate.toLocaleTimeString());
