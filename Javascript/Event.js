5da4a694c5c1862fb2d04a9340e73c522696f87c
prashanthkulal12345 <kulalprashanth779@gmail.com>
2026-07-04 10:09:58 +0530

    learned about other events

--- Javascript/Event.js
+++ Javascript/Event.js
@@ -5,17 +5,22 @@ function clicked() {
 window.onload = function () {
     console.log("Document was loaded");
 
+    let previousHtml=document.querySelectorAll('.container')[0].innerHTML;
+
     const firstcontainer = document.getElementById("firstcontainer");
 
     firstcontainer.addEventListener("click", function () {
+        document.querySelectorAll('.container')[0].innerHTML ="<b>Button was clicked</b>";
         console.log("Clicked on container");
     });
 
     firstcontainer.addEventListener("mouseover", function () {
+        document.querySelectorAll('.container')[0].innerHTML ="<b>Button was clicked</b>";
         console.log("Mouse was moved over the container");
     });
 
     firstcontainer.addEventListener("mouseout", function () {
+        document.querySelectorAll('.container')[0].innerHTML = previousHtml;
         console.log("Mouse was moved out of the container");
     });
 
@@ -24,6 +29,7 @@ window.onload = function () {
     });
 
     firstcontainer.addEventListener("mousedown", function () {
+        document.querySelectorAll('.container')[0].innerHTML ="<b>Button was clicked</b>";
         console.log("Mouse button was pressed");
     });
 };
